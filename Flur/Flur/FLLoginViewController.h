9ff8c0ba2d41939c10167ef4478ed44a48a4b3d1
Dave Lee <davmlee@umich.edu>
2014-11-06 15:36:41 -0500

    login view

---
+++ Flur/Flur/FLLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLLoginViewController.h
+//  Flur
+//
+//  Created by David Lee on 11/6/14.
+//  Copyright (c) 2014 Steve Zuckerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLLoginViewController : UIViewController
+
+@end
---
+++ Flur/Flur/FLLoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  FLLoginViewController.m
+//  Flur
+//
+//  Created by David Lee on 11/6/14.
+//  Copyright (c) 2014 Steve Zuckerman. All rights reserved.
+//
+
+#import "FLLoginViewController.h"
+
+@interface FLLoginViewController ()
+
+@end
+
+@implementation FLLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
