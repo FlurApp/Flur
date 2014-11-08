62b4f2b558ee5a9499f559a44809eb9e78fd1c93
Dave Lee <davmlee@umich.edu>
2014-11-08 14:12:06 -0500

    splashy

---
+++ Flur/FLSplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLSplashViewController.h
+//  Flur
+//
+//  Created by David Lee on 11/8/14.
+//  Copyright (c) 2014 Steve Zuckerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLSplashViewController : UIViewController
+
+@end
---
+++ Flur/FLSplashViewController.m
@@ -0,0 +1,37 @@
+//
+//  FLSplashViewController.m
+//  Flur
+//
+//  Created by David Lee on 11/8/14.
+//  Copyright (c) 2014 Steve Zuckerman. All rights reserved.
+//
+
+#import "FLSplashViewController.h"
+
+@interface FLSplashViewController ()
+
+@end
+
+@implementation FLSplashViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
