5a3e37ec47f3475701f42500cfab12c9706df039
Dave Lee <davmlee@umich.edu>
2014-11-12 20:07:16 -0500

    new view

---
+++ Flur/FLContributedListViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLContributedListViewController.h
+//  Flur
+//
+//  Created by David Lee on 11/12/14.
+//  Copyright (c) 2014 Steve Zuckerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLContributedListViewController : UIViewController
+
+@end
---
+++ Flur/FLContributedListViewController.m
@@ -0,0 +1,37 @@
+//
+//  FLContributedListViewController.m
+//  Flur
+//
+//  Created by David Lee on 11/12/14.
+//  Copyright (c) 2014 Steve Zuckerman. All rights reserved.
+//
+
+#import "FLContributedListViewController.h"
+
+@interface FLContributedListViewController ()
+
+@end
+
+@implementation FLContributedListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
