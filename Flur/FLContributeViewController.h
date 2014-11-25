1586e75e93c76d6fc8ef287a543884804ebf1893
Dave Lee <davmlee@umich.edu>
2014-11-24 20:47:27 -0500

    accidentally dleted the contribute view

---
+++ Flur/FLContributeViewController.h
@@ -0,0 +1,15 @@
+//
+//  FLContributeViewController.h
+//  Flur
+//
+//  Created by David Lee on 11/16/14.
+//  Copyright (c) 2014 Steve Zuckerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLContributeViewController : UIViewController
+- (id)initWithData:(NSMutableDictionary *)data;
+
+
+@end
